9c0fe515ac564cff7a2af0160c17af8f9e6d5946
Alex Chubaty <alex.chubaty@gmail.com>
2014-12-04 13:57:05 -0800

    added climate datastuff
    * new R project (with packrat)
    * script to dl map data
        - **NOTE** ftp links are not permanent!

---
+++ climate-maps/CANESM2_HISTORICAL.R
@@ -0,0 +1,77 @@
+## Libaries
+library(sp)
+library(raster)
+library(RCurl)
+library(rgdal)
+library(rgeos)
+library(snow)
+
+## Set options
+maps.dir <- "M:/data/climate/CANESM2_HISTORICAL"
+#maps.dir = "~/Documents/Data/maps"
+if(!file.exists(maps.dir)) stop("maps dir does not exist.")
+
+scenarios = c("rcp26", "rcp45", "rcp85")
+years = as.character(2006L:2100L)
+
+download = FALSE
+num.cpus = 4
+rasterOptions(maxmemory=1e9, chunksize=1e8)
+
+## Study Region (process the entire conutry minus the territories)
+getOGR <- function(layer, dir) {
+  orig.dir = getwd()
+  setwd(dir)
+  out = readOGR(dsn=".", layer=layer)
+  setwd(orig.dir)
+  return(out)
+}
+boreal = getOGR("NABoreal", file.path(maps.dir, "boreal"))
+boreal.can = boreal[boreal$COUNTRY=="CANADA",]
+crs.boreal = CRS(proj4string(boreal))
+study.region = c("British Columbia", "Alberta", "Saskatchewan", "Manitoba",
+                 "Ontario", "Québec", "New Brunswick", "Nova Scotia",
+                 "Newfoundland", "Prince Edward Island")
+
+# provicial boundaries
+load(file.path(maps.dir, "CAN_adm1.RData"))
+canada1 = gadm
+canada1.boreal = spTransform(canada1, crs.boreal)
+SR.boreal = canada1.boreal[na.omit(match(study.region, canada1.boreal$NAME_1)),]
+rm(gadm, canada1)
+
+# study area (correct for non-adjacent boundaries)
+SR.boreal.union.buff = gBuffer(SR.boreal, width=1e-5)
+boreal.SR = intersect(SR.boreal.union.buff, boreal.can)
+rm(SR.boreal.union.buff)
+
+## Data directories
+tmpdir = tmpDir()
+
+if (download) {
+  ## Get files of size zero
+  size.zero <- function(path=".", size=0) {
+    files = dir(path, full.names=TRUE, recursive=TRUE)
+    return(files[file.info(files)$size==size])
+  }
+  
+  eol = ifelse(Sys.info()[["sysname"]]=="Windows", "\r\n", "\n")
+  
+  ## Define the ftp address
+  ftpsite <- "ftp://ftp.nrcan.gc.ca/pub/outgoing/CANESM2_HISTORICAL/"
+  
+  for (i in scenarios) { 
+    for (j in years) {
+      ftppath = paste0(ftpsite, i, "/asciigrids/", j, "/")
+      locpath = file.path(maps.dir, i, "asciigrids", j)
+      if(!file.exists(locpath)) dir.create(locpath, recursive=TRUE)
+      
+      files = unlist(strsplit(getURL(ftppath, dirlistonly=TRUE), split=eol))
+      
+      for (k in files) {
+        try(download.file(paste0(ftppath, k), file.path(locpath, k)))
+      }
+    }
+  }
+}
+
