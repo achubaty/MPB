0b2c2de8be30c3834baac20f64231f191618cf9d
Alex Chubaty <alex.chubaty@gmail.com>
2015-01-07 14:27:33 -0800

    updated plotting colours
    using `RColorBrewer`

--- climate-maps/CANESM2.R
+++ climate-maps/CANESM2.R
@@ -1,5 +1,6 @@
 library(chron)
 library(ncdf4)
+library(RColorBrewer)
 
 if (Sys.info()[["sysname"]]=="Windows") {
   path <- file.path("//W-VIC-A105254/shared/data/climate/CANESM2/NetCDF_files")
@@ -17,15 +18,15 @@ for (d in dirs) {
     
     print(nc)
     
-    latitude <- ncvar_get(nc, varid="latitude")
-    longitude <- ncvar_get(nc, varid="longitude")
+    lat <- ncvar_get(nc, varid="latitude")
+    long <- ncvar_get(nc, varid="longitude")
     
     time <- ncvar_get(nc, varid="time")
     t <- chron(time, origin=c(month=12, day=31, year=1951))
     
     vals <- ncvar_get(nc, varid="swind_anom") ###
     
-    image(longitude, rev(latitude), vals[, ncol(vals):1, 660]) #
+    image(long, rev(lat), vals[, ncol(vals):1, 660], col=rev(brewer.pal(10, "RdBu"))) #
     
     nc_close(f)
   }  
