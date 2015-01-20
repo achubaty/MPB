1fe14aa192b8d1a159434556925ebb2e6fca20ac
Alex Chubaty <alex.chubaty@gmail.com>
2015-01-20 11:42:44 -0800

    minor whitespace clean up

--- climate-maps/CANESM2.R
+++ climate-maps/CANESM2.R
@@ -12,22 +12,22 @@ setwd(path)
 dirs <- dir(path)
 for (d in dirs) {
   ncFiles <- dir(file.path(path, d), pattern="[.]nc$", full.names=TRUE)
-  
+
   for (f in ncFiles) {
     nc <- nc_open(f)
-    
+
     print(nc)
-    
+
     lat <- ncvar_get(nc, varid="latitude")
     long <- ncvar_get(nc, varid="longitude")
-    
+
     time <- ncvar_get(nc, varid="time")
     t <- chron(time, origin=c(month=12, day=31, year=1951))
-    
+
     vals <- ncvar_get(nc, varid="swind_anom") ###
-    
+
     image(long, rev(lat), vals[, ncol(vals):1, 660], col=rev(brewer.pal(10, "RdBu"))) #
-    
+
     nc_close(f)
-  }  
+  }
 }
