15c98bafcfd387ca39469314882d80f0547dbfc1
Alex Chubaty <alex.chubaty@gmail.com>
2016-10-31 16:56:14 -0600

    update cded script

--- dem-maps/Canadian_Digital_Elevation_Data.R
+++ dem-maps/Canadian_Digital_Elevation_Data.R
@@ -1,13 +1,15 @@
 ## Set options
-os = tolower(Sys.info()[["sysname"]])
-maps.dir = if (os=="windows") {
-  "//W-VIC-A105254/shared/data"
-} else if (os=="Darwin") {
-  "~/Documents/Data/maps"
-} else if (os=="linux") {
-  "/mnt/A105254/shared/data"
+CFS <- grepl("W-VIC", Sys.info()[["nodename"]])
+OS <- Sys.info()[["sysname"]]
+
+maps.dir <- if (OS == "Windows") {
+  "//W-VIC-A105388/shared/data"
+} else if (OS == "Darwin") {
+  "~/Documents/shared"
+} else if (OS == "Linux") {
+  ifelse(isTRUE(CFS), "/mnt/A105388/shared/data", "~/Documents/shared")
 }
-if(!file.exists(maps.dir)) stop("maps.dir does not exist.")
+if (!dir.exists(maps.dir)) stop("maps.dir does not exist.")
 
 download50k = FALSE
 download250k = FALSE
@@ -18,8 +20,9 @@ reprocess250k = FALSE
 num.cpus = 4
 
 ## Libaries
+library(devtools)
 library(sp)
-library(raster); rasterOptions(maxmemory=2e9, chunksize=5e7)
+library(raster); rasterOptions(maxmemory = 2e9, chunksize = 5e7)
 library(rgdal)
 library(rgeos)
 library(snow)
@@ -35,23 +38,11 @@ tmpdir250k = file.path(tmpdir, "250k_dem")
 
 ## Fetch elevation data from internet
 if (download50k) {
-  if (os=="windows") {
-    system("cd ~/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/GitHub/McIntire-lab/code/data-sources/cded-download-50k.R")
-  } else {
-    system("cd ~/Documents/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/Documents/GitHub/McIntire-lab/code/data-sources/cded-download-50k.R")
-  }
+  source_url("https://raw.githubusercontent.com/eliotmcintire/McIntire-lab/master/code/data-sources/cded-download-50k.R")
 }
 
 if (download250k) {
-  if (os=="windows") {
-    system("cd ~/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/GitHub/McIntire-lab/code/data-sources/cded-download-250k.R")
-  } else {
-    system("cd ~/Documents/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/Documents/GitHub/McIntire-lab/code/data-sources/cded-download-250k.R")
-  }
+  source_url("https://raw.githubusercontent.com/eliotmcintire/McIntire-lab/master/code/data-sources/cded-download-250k.R")
 }
 
 ## Reprocess each ToI and reproject for new study area
@@ -59,35 +50,23 @@ if (download250k) {
 ## NOTE: be sure to set options, including study region definition within these files!
 ##
 if (reprocess50k) {
-  if (os=="windows") {
-    system("cd ~/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/GitHub/McIntire-lab/code/data-sources/cded-reprocess-50k.R")
-  } else {
-    system("cd ~/Documents/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/Documents/GitHub/McIntire-lab/code/data-sources/cded-reprocess-50k.R")
-  }
+  source_url("https://raw.githubusercontent.com/eliotmcintire/McIntire-lab/master/code/data-sources/cded-reprocess-50k.R")
 }
 
 if (reprocess250k) {
-  if (os=="windows") {
-    system("cd ~/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/GitHub/McIntire-lab/code/data-sources/cded-reprocess-250k.R")
-  } else {
-    system("cd ~/Documents/GitHub/McIntire-lab && git pull", intern=TRUE, wait=TRUE)
-    source("~/Documents/GitHub/McIntire-lab/code/data-sources/cded-reprocess-250k.R")
-  }
+  source_url("https://raw.githubusercontent.com/eliotmcintire/McIntire-lab/master/code/data-sources/cded-reprocess-250k.R")
 }
 
 ## Load previously saved DEM objects
 load(file.path(maps.dir, "cded", "dem_SR_boreal_250k.RData"))
 
 ##------------------------------------------------------------------------------
-##
+## from Jean; incomplete!
 ##------------------------------------------------------------------------------
-srRt_100m <- disaggregate(srRt, fact=100)
+srRt_100m <- disaggregate(srRt, fact = 100)
 
-dem_250k_100m <- projectRaster(from=dem_250k, to=srRt_100m)
-dem_50k_100m <- projectRaster(from=dem_50k, to=srRt_100m)
+dem_250k_100m <- projectRaster(from = dem_250k, to = srRt_100m)
+dem_50k_100m <- projectRaster(from = dem_50k, to = srRt_100m)
 
-sd_dem_250k <- aggregate(dem_250k_100m, fact=100, fun=sd)
-sd_dem_50k <- aggregate(dem_50k_100m, fact=100, fun=sd)
+sd_dem_250k <- aggregate(dem_250k_100m, fact = 100, fun = sd)
+sd_dem_50k <- aggregate(dem_50k_100m, fact = 100, fun = sd)
