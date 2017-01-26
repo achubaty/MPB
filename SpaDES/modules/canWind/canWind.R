eba7be2f0e0710bc2b8b001d738361d106559542
Alex Chubaty <alex.chubaty@gmail.com>
2017-01-26 15:50:28 -0700

    rename mpbWind to canWind
    this will be a more general module!

--- SpaDES/modules/canWind/canWind.R
+++ SpaDES/modules/canWind/canWind.R
@@ -2,8 +2,8 @@
 # Everything in this file gets sourced during simInit, and all functions and objects
 # are put into the simList. To use objects and functions, use sim$xxx.
 defineModule(sim, list(
-  name = "mpbWind",
-  description = "insert module description here",
+  name = "canWind",
+  description = "Simulate mean annual wind speeds/directions by generating random directions (biased eastward).",
   keywords = c("insert key words here"),
   authors = person(c("Alex", "M"), "Chubaty", email = "alexander.chubaty@canada.ca", role = c("aut", "cre")),
   childModules = character(0),
@@ -12,8 +12,8 @@ defineModule(sim, list(
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
   citation = list("citation.bib"),
-  documentation = list("README.txt", "mpbWind.Rmd"),
-  reqdPkgs = list(),
+  documentation = list("README.txt", "canWind.Rmd"),
+  reqdPkgs = list("CircStats", "raster"),
   parameters = rbind(
     #defineParameter("paramName", "paramClass", value, min, max, "parameter description")),
     defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
@@ -23,40 +23,34 @@ defineModule(sim, list(
     defineParameter(".useCache", "numeric", FALSE, NA, NA, "Should this entire module be run with caching activated?")
   ),
   inputObjects = bind_rows(
-    #expectsInput("objectName", "objectClass", "input object description", sourceURL, ...),
-    expectsInput(objectName = NA, objectClass = NA, desc = NA, sourceURL = NA)
+    expectsInput(objectName = "studyArea", objectClass = "SpatialPolygons",
+                 desc = "The study area to which all maps will be cropped and reprojected.", sourceURL = NA)
   ),
   outputObjects = bind_rows(
-    #createsOutput("objectName", "objectClass", "output object description", ...),
-    createsOutput(objectName = NA, objectClass = NA, desc = NA)
+    createsOutput(objectName = "windMap", objectClass = "RasterStack",
+                  desc = "Raster maps corresponding to wind spped and direction.")
   )
 ))
 
 ## event types
 #   - type `init` is required for initialiazation
 
-doEvent.mpbWind <- function(sim, eventTime, eventType, debug = FALSE) {
+doEvent.canWind <- function(sim, eventTime, eventType, debug = FALSE) {
   switch(eventType,
     "init" = {
-      ### check for more detailed object dependencies:
-      ### (use `checkObject` or similar)
-  
       # do stuff for this event
-      sim <- sim$mpbWindInit(sim)
+      sim <- sim$canWindInit(sim)
   
       # schedule future event(s)
-      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "mpbWind", "plot")
-      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "mpbWind", "save")
+      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "canWind", "plot")
     },
     "plot" = {
       # ! ----- EDIT BELOW ----- ! #
       # do stuff for this event
-  
-      #Plot(objectFromModule) # uncomment this, replace with object to plot
+      Plot(sim$windMap)
+      
       # schedule future event(s)
-  
-      # e.g.,
-      #sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "mpbWind", "plot")
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "canWind", "plot")
   
       # ! ----- STOP EDITING ----- ! #
     },
@@ -82,7 +76,7 @@ doEvent.mpbWind <- function(sim, eventTime, eventType, debug = FALSE) {
   # }
   # ! ----- EDIT BELOW ----- ! #
   if (!('studyArea' %in% sim$.userSuppliedObjNames)) {
-    load(file.path(modulePath(sim), "mpbWind", "data", "west.boreal.RData"), envir = envir(sim))
+    load(file.path(modulePath(sim), "canWind", "data", "west.boreal.RData"), envir = envir(sim))
   }
   
   # ! ----- STOP EDITING ----- ! #
@@ -95,19 +89,8 @@ doEvent.mpbWind <- function(sim, eventTime, eventType, debug = FALSE) {
 #   - keep event functions short and clean, modularize by calling subroutines from section below.
 
 ### template initialization
-mpbWindInit <- function(sim) {
+canWindInit <- function(sim) {
   # # ! ----- EDIT BELOW ----- ! #
-  
-  # ! ----- STOP EDITING ----- ! #
-  
-  return(invisible(sim))
-}
-
-### template for plot events
-mpbWindPlot <- function(sim) {
-  # ! ----- EDIT BELOW ----- ! #
-  # do stuff for this event
-  #Plot("object")
   
   # ! ----- STOP EDITING ----- ! #
   return(invisible(sim))
