8839bbfb280da5fd6b4db79b27bd3ee4a70aac1a
Alex Chubaty <alex.chubaty@gmail.com>
2017-04-20 20:39:15 -0600

    remove unused app file

--- app/mapsForShiny.R
+++
@@ -1,6 +0,0 @@
-## ensure global parameters exist (from global.R)
-stopifnot(exists(c("paths")))
-
-
-## -----------------------------------------------------------------------------
-message(brk(), "  finished running mapsForShiny.R [", Sys.time(), "]", "\n", brk())
