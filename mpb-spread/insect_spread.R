771bf3b505a57f36bd7a945f44a3d9ef06862ca7
Eliot McIntire <eliotmcintire@gmail.com>
2018-01-16 14:11:35 -0800

    oh raster package ... MAJOR speedup

--- mpb-spread/insect_spread.R
+++ mpb-spread/insect_spread.R
@@ -7,19 +7,19 @@ library(SpaDES.tools)
 
 ._NUMCPUS_. <- parallel::detectCores() / 2
 
-dev.useRSGD(FALSE) 
+dev.useRSGD(FALSE)
 dev()
 
 # inputs
 XMAX <- YMAX <- 400
 a <- raster(extent(0, XMAX, 0, YMAX), res = 1)
 pine <- gaussMap(a) ## gaussMap resets DT threads, so be sure to reset it below
-pine[] <- pine[] / maxValue(pine) 
+pine[] <- pine[] / maxValue(pine)
 #pine[] <- 0.6 ## TEMPORARY
 
 setDTthreads(._NUMCPUS_.)
 
-# parameter in the dispKern function, describes steepness of curve, 
+# parameter in the dispKern function, describes steepness of curve,
 #  higher towards 1 is steeper, lower towards 0 is flatter
 LAMBDA <- 0.12
 
@@ -30,7 +30,7 @@ dispKern <- function(disFar, disNear, lambda) {
 loci <- sample(ncell(a), 100)
 
 ## max num red trees [= 1125 trees/ha * (MAPRES / 100)^2]
-TOTAL <- round(1125 * (250 / 100)^2) 
+TOTAL <- round(1125 * (250 / 100)^2)
 saturationDensity <- 332
 asymm <- 5
 
@@ -57,17 +57,17 @@ system.time({
         # might spread and have none that are <1 unit. Just go back to while loop
         set(out, , "order", seq_len(NROW(out)))
         attribs <- attr(out, "spreadState")
-        
+
         setkey(out, initialPixels, from)
         out[, beetleSource := !is.na(abundanceActive),  by = c("initialPixels", "from")]
         receivers <- out[beetleSource == FALSE]
         sources <- out[beetleSource == TRUE]
-        
+
         setkey(sources, initialPixels, pixels)
         setkey(receivers, initialPixels, from)
         outW_i_columns <- receivers[sources]
         outWLag1B <- outW_i_columns[beetleSource == FALSE]
-        
+
         # Calculate the abundance received, as a function of distance
         outWLag1B[, abundanceReceived :=
                     pmin(i.Total, ceiling(dispKern(effectiveDistance, i.effectiveDistance, LAMBDA) *
@@ -77,22 +77,22 @@ system.time({
                   by = c("initialPixels", "from")] # Extra ones if they didn't settle
         # Calculate the abundance received, as a function of angle,
         # which was already calculated in spread2, and is called "proportion".
-        # The pmin is about saturation density. 
-        outWLag1B[, abundanceSettled := pmin(floor(abundanceReceived * pine[pixels] *
+        # The pmin is about saturation density.
+        outWLag1B[, abundanceSettled := pmin(floor(abundanceReceived * pine[][pixels] *
                                                      saturationDensity /
-                                                     sum(abundanceReceived * pine[pixels])),
-                                             ceiling(abundanceReceived * pine[pixels])),
+                                                     sum(abundanceReceived * pine[][pixels])),
+                                             ceiling(abundanceReceived * pine[][pixels])),
                   by = c("pixels")]
         outWLag1B[, abundanceActive := pmin(i.Total, floor(abundanceReceived - abundanceSettled))]
-        
+
         set(outWLag1B, , grep(colnames(outWLag1B), pattern = "^i\\.", value = TRUE), NULL)
-        
+
         out <- rbindlist(list(sources, outWLag1B), fill = TRUE)
-        
+
         # Squash down duplicates
-        outSum <- out[, list(abundanceActive = sum(abundanceActive), 
-                             abundanceSettled = sum(abundanceSettled), 
-                             abundanceReceived = sum(abundanceReceived)), 
+        outSum <- out[, list(abundanceActive = sum(abundanceActive),
+                             abundanceSettled = sum(abundanceSettled),
+                             abundanceReceived = sum(abundanceReceived)),
                       by = c("initialPixels", "pixels")]
         out <- unique(out, by = c("initialPixels", "pixels"))
         set(out, , "abundanceActive", outSum$abundanceActive)
@@ -100,8 +100,8 @@ system.time({
         set(out, , "abundanceReceived", outSum$abundanceReceived)
         set(out, , "Total", TOTAL)
         setattr(out, "spreadState", attribs)
-        
-        # Because of multiple starting loci, a given pixel can get overfull. 
+
+        # Because of multiple starting loci, a given pixel can get overfull.
         overfull <- out[, sum(abundanceSettled), by = "pixels"]
         if (isTRUE(any(overfull$V1 > saturationDensity))) {
           pixelsWithTooMany <- overfull[V1 > saturationDensity]$pixels
@@ -117,15 +117,15 @@ system.time({
                 (out$abundanceSettled[cumsumAboveSaturation] - abundanceSettledTemp))
           set(out, cumsumAboveSaturation, "abundanceSettled", abundanceSettledTemp)
         }
-        
+
         if (all(out[, sum(abundanceSettled, na.rm = TRUE) >= unique(Total), by = "initialPixels"])) {
           done <- TRUE
         }
 
         ## print debugging/status info
-        print(paste(attr(out, "spreadState")$totalIterations, 
-                    paste(out[, sum(abundanceSettled, na.rm = TRUE) >= unique(Total), 
-                              by = "initialPixels"]$V1, 
+        print(paste(attr(out, "spreadState")$totalIterations,
+                    paste(out[, sum(abundanceSettled, na.rm = TRUE) >= unique(Total),
+                              by = "initialPixels"]$V1,
                     collapse = ", ")))
       }
     }
@@ -135,7 +135,7 @@ system.time({
                        abundanceReceived = sum(abundanceReceived)), by = c("pixels")]
     aa[out1$pixels] <- out1$abundanceSettled
     #aa[out1$pixels] <- out1$abundanceReceived
-    
+
     clearPlot()
     Plot(aa, zero.color = "red", new = TRUE)
   }
